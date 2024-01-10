96a8048d42bd4be2abc836c4da35aa411599d9d8
xiaofu <xiaofu@dtstack.com>
2024-01-11 02:48:06 +0800

    [feat_1.0.x_xiaofu] [刷题模块准备]

--- study-subject/application/subject-application-controller/src/main/java/com/xiaofu/subject/application/controller/SubjectController.java
+++ study-subject/application/subject-application-controller/src/main/java/com/xiaofu/subject/application/controller/SubjectController.java
@@ -2,7 +2,6 @@ package com.xiaofu.subject.application.controller;
 
 import com.xiaofu.subject.infra.basic.entity.SubjectCategory;
 import com.xiaofu.subject.infra.basic.service.SubjectCategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- study-subject/infra/src/main/java/com/xiaofu/subject/infra/basic/entity/SubjectCategory.java
+++ study-subject/infra/src/main/java/com/xiaofu/subject/infra/basic/entity/SubjectCategory.java
@@ -1,137 +1,63 @@
 package com.xiaofu.subject.infra.basic.entity;
 
-import java.util.Date;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.time.LocalDateTime;
 import java.io.Serializable;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
 
 /**
- * 题目分类表(SubjectCategory)实体类
+ * <p>
+ * 题目分类表
+ * </p>
  *
  * @author xiaofu
- * @since 2024-01-10 22:56:27
+ * @since 2024-01-11
  */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("subject_category")
+@ApiModel(value="SubjectCategory对象", description="题目分类表")
 public class SubjectCategory implements Serializable {
-    private static final long serialVersionUID = -77845100931258658L;
-    /**
-     * 主键
-     */
+
+    private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "主键")
+    @TableId(value = "id", type = IdType.AUTO)
     private Long id;
-    /**
-     * 分类名称
-     */
+
+    @ApiModelProperty(value = "分类名称")
     private String categoryName;
-    /**
-     * 分类类型
-     */
+
+    @ApiModelProperty(value = "分类类型")
     private Integer categoryType;
-    /**
-     * 图标连接
-     */
+
+    @ApiModelProperty(value = "图标连接")
     private String imageUrl;
-    /**
-     * 父级id
-     */
+
+    @ApiModelProperty(value = "父级id")
     private Long parentId;
-    /**
-     * 创建人
-     */
+
+    @ApiModelProperty(value = "创建人")
     private String createdBy;
-    /**
-     * 创建时间
-     */
-    private Date createdTime;
-    /**
-     * 更新人
-     */
+
+    @ApiModelProperty(value = "创建时间")
+    private LocalDateTime createdTime;
+
+    @ApiModelProperty(value = "更新人")
     private String updateBy;
-    /**
-     * 更新时间
-     */
-    private Date updateTime;
-    /**
-     * 是否删除 0: 未删除 1: 已删除
-     */
-    private Integer isDeleted;
 
+    @ApiModelProperty(value = "更新时间")
+    private LocalDateTime updateTime;
 
-    public Long getId() {
-        return id;
-    }
+    @ApiModelProperty(value = "是否删除 0: 未删除 1: 已删除")
+    private Boolean isDeleted;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public Integer getCategoryType() {
-        return categoryType;
-    }
-
-    public void setCategoryType(Integer categoryType) {
-        this.categoryType = categoryType;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public Long getParentId() {
-        return parentId;
-    }
-
-    public void setParentId(Long parentId) {
-        this.parentId = parentId;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public Date getCreatedTime() {
-        return createdTime;
-    }
-
-    public void setCreatedTime(Date createdTime) {
-        this.createdTime = createdTime;
-    }
-
-    public String getUpdateBy() {
-        return updateBy;
-    }
-
-    public void setUpdateBy(String updateBy) {
-        this.updateBy = updateBy;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-
-    public Integer getIsDeleted() {
-        return isDeleted;
-    }
-
-    public void setIsDeleted(Integer isDeleted) {
-        this.isDeleted = isDeleted;
-    }
 
 }
-
