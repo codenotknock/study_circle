f2dba1450ea6ab080d0cf73232549cf768e92607
xiaofu <13781678921@163.com>
2024-01-13 02:02:08 +0800

    [feat_1.0.x_xiaofu] [刷题之题目标签模块] 根据题目分类id查找对应标签

---
+++ study-subject/infra/src/main/java/com/xiaofu/subject/infra/basic/service/impl/SubjectMappingServiceImpl.java
@@ -0,0 +1,28 @@
+package com.xiaofu.subject.infra.basic.service.impl;
+
+import com.xiaofu.subject.infra.basic.entity.SubjectMapping;
+import com.xiaofu.subject.infra.basic.mapper.SubjectMappingMapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.xiaofu.subject.infra.basic.service.SubjectMappingService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * <p>
+ * 题目分类关系表 服务实现类
+ * </p>
+ *
+ * @author xiaofu
+ * @since 2024-01-13
+ */
+@Service
+public class SubjectMappingServiceImpl extends ServiceImpl<SubjectMappingMapper, SubjectMapping> implements SubjectMappingService {
+
+    @Override
+    public List<SubjectMapping> queryLabelIdsByCategoryId(Long categoryId) {
+        return this.lambdaQuery()
+                .eq(SubjectMapping::getCategoryId, categoryId)
+                .list();
+    }
+}
