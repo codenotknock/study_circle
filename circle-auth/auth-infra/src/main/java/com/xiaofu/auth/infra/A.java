8c97a04b9a3c24ead97648e85a92e76796b73f19
xiaofu <13781678921@163.com>
2024-01-15 22:28:15 +0800

    [feat_1.0.x_xiaofu] [鉴权模块] 依赖引入

---
+++ circle-auth/auth-infra/src/main/java/com/xiaofu/auth/infra/A.java
@@ -0,0 +1,9 @@
+package com.xiaofu.auth.infra;
+
+/**
+ * @author xiaofu
+ * @date 2024/1/15 22:18
+ * @des
+ */
+public class A {
+}
---
+++ circle-auth/auth-starter/src/main/java/com/xiaofu/auth/AuthApplication.java
@@ -0,0 +1,16 @@
+package com.xiaofu.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xiaofu
+ * @date 2024/1/15 22:16
+ * @des
+ */
+@SpringBootApplication
+public class AuthApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+}
