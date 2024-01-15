d923ea63ee4882af292ed9c9cdf91190936f1ca0
xiaofu <13781678921@163.com>
2024-01-15 23:11:39 +0800

    [feat_1.0.x_xiaofu] [鉴权模块] saToken demo测试

--- circle-auth/auth-starter/src/main/java/com/xiaofu/auth/AuthApplication.java
+++ circle-auth/auth-starter/src/main/java/com/xiaofu/auth/AuthApplication.java
@@ -2,12 +2,14 @@ package com.xiaofu.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author xiaofu
  * @date 2024/1/15 22:16
  * @des
  */
+@ComponentScan("com.xiaofu.auth")
 @SpringBootApplication
 public class AuthApplication {
     public static void main(String[] args) {
