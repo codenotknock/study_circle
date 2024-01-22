684a4777af5bf6f6801d85c82533ff3f93824c42
codenotknock <13781678921@163.com>
2024-01-22 22:31:00 +0800

    [feat_1.0.x_xiaofu] [公共代码优化 并上传到云效制品库]

---
+++ circle-common-starter/src/main/java/com/xiaofu/common/entity.java
@@ -0,0 +1,9 @@
+package com.xiaofu.common;
+
+/**
+ * @author xiaofu
+ * @date 2024/1/19 21:42
+ * @des
+ */
+public class entity {
+}
---
+++ circle-common-starter/src/main/java/com/xiaofu/common/enums/a.java
@@ -0,0 +1,9 @@
+package com.xiaofu.common.enums;
+
+/**
+ * @author xiaofu
+ * @date 2024/1/19 15:39
+ * @des
+ */
+public class a {
+}
